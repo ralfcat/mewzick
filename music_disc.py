dc9f3087febd7dadf76d67180834ed5c8141c24d
ralfcat <victor.englof@hotmail.com>
2024-02-22 21:32:33 +0100

    Merge branch 'main' of https://github.com/ralfcat/music_disc

--- music_disc.py
+++ music_disc.py
@@ -544,4 +544,4 @@ class MusicQuizControls(View):
 # When initializing MusicQuizControls, also call add_buttons to add the option buttons
 
 ### TOKEN TO RUN THE BOT ####
-bot.run('MTIwMDgzMDQ5NTAyMjM5OTYwOQ.Ga1wcT.1cX3uvxNtoVKzZJYx1mb-sHMT_7xBx_BLZDKZI')
+bot.run('MTIwMDgzMDQ5NTAyMjM5OTYwOQ.GVaJVV.UTjbUbukX9PvnwMbXjmYk7Ef1LFBkuSMdxlUx8')
